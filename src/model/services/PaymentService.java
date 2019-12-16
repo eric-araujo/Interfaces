558d516f087e4b9673cd567cf24e2ec93a210f7d
Eric Araújo <eric.araujolima@hotmail.com>
2019-12-16 19:38:05 -0300

    Camada de Serviços

---
+++ src/model/services/PaymentService.java
@@ -0,0 +1,12 @@
+package model.services;
+
+public class PaymentService implements OnlinePaymentService{
+
+	public Double paymentFee(Double amount) {
+		return null;
+	}
+	
+	public Double interest(Double amount, Integer months) {
+		return null;
+	}
+}
